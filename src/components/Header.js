b4c0c496c50328e8a1bbdccf776e30d70e025a0c
HectorAgHe <aguilarhector440@gmail.com>
2023-12-14 20:57:48 -0600

    Header_icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
     <>
       <header className="header-top-strip py-3">
         <div className="container-xxl">
-          <div className="row">
+          <div className="row align-items-center">
             <div className="col-6">
               <p className="text-white mb-0">
                 Free Shipping  Over $100 & Free Return
@@ -33,21 +33,55 @@ const Header = () => {
             </h2>
           </div>
           <div className="col-5">
-            <div class="input-group mb-3">
+            <div className="input-group">
               <input
                 type="text"
-                class="form-control"
+                className="form-control py-2"
                 placeholder="Search Product Here..."
                 aria-label="Search Product Here..."
                 aria-describedby="basic-addon2" />
-              <span class="input-group-text" id="basic-addon2">Search</span>
+              <span class="input-group-text p-3" id="basic-addon2">
+                <BsSearch className="fs-6"/>
+                </span>
             </div>
           </div>
-          <div className="col-5"></div>
+          <div className="col-5">
+            <div className="header-upper-links d-flex align-items-center justify-content-betwen">
+              <div>
+               <Link>
+                <img src="/images/compare.svg" alt="compare" />
+                <p>Compare <br /> Products</p>
+               </Link>
+              </div>
+              
+              <div>
+               <Link>
+                <img src="/images/wishList.svg" alt="wishlist" />
+                <p>Favorite <br /> wishList </p>
+               </Link>
+              </div>
+
+              <div>
+               <Link>
+                <img src="/images/user.svg" alt="" />
+                <p>Log in<br /> My Account</p>
+               </Link>
+              </div>
+              <div>
+               <Link className="d-flex align-items-center gap">
+                <img src="/images/cart.svg" alt="cart" />
+                <div className= "d-flex flex-column">
+                  <span className="badge bg-white text-dark">0</span>
+                </div>
+               </Link>
+              </div>
+
+            </div>
+          </div>
         </div>
       </header>
     </>
-  )
-}
+  );
+};
 
 export default Header
